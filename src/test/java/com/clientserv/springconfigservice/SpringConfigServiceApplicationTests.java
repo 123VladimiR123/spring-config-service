290a05465efc8bdc2debe6bc47b8c17112a6c5dd
123VladimiR123 <888prikol888@gmail.com>
2024-04-04 17:26:24 +0300

    Initial commit

---
+++ src/test/java/com/clientserv/springconfigservice/SpringConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientserv.springconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
